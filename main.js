d4b16cb2cdafbb3669e811d966c3b3660afceca8
Jomari <Jomari8@hawaii.edu>
2025-05-06 15:49:15 -1000

    Style header and added slide show to html

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-function showSlides() 
+const swiper = new Swiper('.swiper', {
+    autoplay: {
+        delay: 6000,
+        disableOnInteraction: false,
+    },
+    loop: true,
+  
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    },
+  );
